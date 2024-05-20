5ac8b9cb8a9a880e29f37bbf9ede17efedbe9ad3
Tadeo <tadeo.am@gmail.com>
2024-05-20 12:25:35 -0400

    Responsive Logo02

--- src/app/components/ui/heroLogo.tsx
+++ src/app/components/ui/heroLogo.tsx
@@ -17,12 +17,12 @@ export const HEROLogo = () =>
         
           
           
-          <div className="abolute    w-screen h-screen bg-[#0d0210]">
+          <div className="abolute   w-screen h-screen bg-[#0d0219]">
 
               <Image 
                 src= {widthCSS >650 ?"/HeroAbstract.svg": "/HeroAbstractSmall.svg"}
                 alt={widthCSS >650 ?"/Brain03Big.png": "/Brain03Small.png"}
-                className=""
+                className=" "
                 
                 priority={true}
                fill={true}
