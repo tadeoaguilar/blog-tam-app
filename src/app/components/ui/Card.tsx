323da6d576814fd03dc2152181716c2f9c422940
Tadeo <tadeo.am@gmail.com>
2024-05-24 20:06:53 -0400

    fsd 07

--- src/app/components/ui/Card.tsx
+++ src/app/components/ui/Card.tsx
@@ -4,7 +4,7 @@ export interface IPropsCard  {
     
     title: string,
     subTitle:string,
-    titleBgColor: "cyan" | "red" | "orange" | "white" |"grape" | "blue"
+    titleBgColor: "text-cyan" | "text-red" | "text-orange" | "text-white" |"text-grape" | "text-blue"
     svgFile: string,
     learn:string
 
@@ -13,7 +13,7 @@ export interface IPropsCard  {
 const Card: React.FC<IPropsCard> = (props) => {
 
   return (
-    <div className={`bg-grape rounded-xl  w-full h-80 text-${props.titleBgColor.trim()} `}>
+    <div className={"bg-grape rounded-xl  w-full h-80 " + props.titleBgColor}>
       <div className='flex flex-col w-5/6 mx-auto h-full py-8'>
         <p className='font-bold text-2xl text-cyan'>{props.title} </p>
         <p className='font-bold text-base pt-4'>{props.subTitle} </p>
