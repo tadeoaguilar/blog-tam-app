cc0bdd722cc1e6525b0e17c86038a3911eafb4a5
Tadeo <tadeo.am@gmail.com>
2024-05-28 11:29:13 -0400

    svg 50

--- src/app/pages/Capabilities.tsx
+++ src/app/pages/Capabilities.tsx
@@ -43,7 +43,7 @@ const HeroText = () => {
 
     return (
         <section className="h-full w-screen sm:h-screen bg-orange bg-opacity-80 mx-auto flex flex-col items-center justify-center ">
-            <div className = "flex flex-col   sm:grid   sm:grid-cols-2 sm:grid-rows-3 md:grid-cols-3 md:grid-rows-2 justify-center mx-auto w-10/12  gap-4 py-2 ">
+            <div className = "h-full bg-cyan flex flex-col   sm:grid   sm:grid-cols-2 sm:grid-rows-3 md:grid-cols-3 md:grid-rows-2 justify-center mx-auto w-10/12  gap-4 py-2 ">
                 {
                 cardData.map( (item,index) =>{
 
